a31b4c49b512922cba06a9590a9cabbb6b1ca0fb
姚利 <yaoli@nesc.cn>
2025-09-12 09:58:21 +0800

    feat:docs中引入codemirror展示示例代码

--- docs/.vitepress/src/vp-source-code.vue
+++ docs/.vitepress/src/vp-source-code.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
-import { computed } from "vue";
+import { computed, onMounted, ref, nextTick } from "vue";
+import { EditorView } from "@codemirror/view";
+import { basicSetup } from "codemirror";
+import { html } from "@codemirror/lang-html";
+import { EditorState } from "@codemirror/state";
 
 const props = defineProps({
   source: {
@@ -8,19 +12,37 @@ const props = defineProps({
   },
 });
 
+const editorRef = ref<HTMLDivElement>();
+let editorView: EditorView | null = null;
+
 const decoded = computed(() => {
   return decodeURIComponent(props.source);
+});
+
+onMounted(async () => {
+  await nextTick();
+  if (editorRef.value) {
+    editorView = new EditorView({
+      state: EditorState.create({
+        doc: decoded.value,
+        extensions: [
+          basicSetup,
+          html(),
+        ],
+      }),
+      parent: editorRef.value,
+    });
+  }
 });
 </script>
 
 <template>
   <div class="example-source-wrapper">
-    <div class="example-source" v-html="decoded" />
+    <div ref="editorRef" class="example-source"></div>
   </div>
 </template>
 
 <style scoped lang="scss">
-
 .example-source{
     overflow-x: scroll;
     position: relative;
